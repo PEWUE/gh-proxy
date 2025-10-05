b6d858a91c2c8d244d7e38b567a42f3d5b715c55
Paweł Kopeć <pawel.kopec13@gmail.com>
2025-10-05 20:57:14 +0200

    initial commit

---
+++ src/main/java/com/pewue/gh_proxy/GhProxyApplication.java
@@ -0,0 +1,13 @@
+package com.pewue.gh_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhProxyApplication.class, args);
+	}
+
+}
