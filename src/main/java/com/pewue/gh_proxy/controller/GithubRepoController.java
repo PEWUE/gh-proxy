e76a161a371d935d08dbc513b072846e3ba6ea7d
Paweł Kopeć <pawel.kopec13@gmail.com>
2025-10-06 11:59:52 +0200

    clean up

--- src/main/java/com/pewue/gh_proxy/controller/GithubRepoController.java
+++ src/main/java/com/pewue/gh_proxy/controller/GithubRepoController.java
@@ -1,8 +1,6 @@
 package com.pewue.gh_proxy.controller;
 
-import com.pewue.gh_proxy.client.GitHubClient;
 import com.pewue.gh_proxy.dto.RepoDetailsDto;
-import com.pewue.gh_proxy.mapper.RepoDetailsMapper;
 import com.pewue.gh_proxy.service.GithubRepoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/repositories")
 public class GithubRepoController {
-    private final GitHubClient gitHubClient;
     private final GithubRepoService githubRepoService;
-    private final RepoDetailsMapper mapper;
 
     @GetMapping("/{owner}/{repo}")
     public RepoDetailsDto getRepositoryDetails(@PathVariable String owner, @PathVariable String repo) {
