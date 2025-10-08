f38270c5a818add1aebc2d812ad55d9cb5c21b24
Paweł Kopeć <pawel.kopec13@gmail.com>
2025-10-08 16:10:23 +0200

    add retry to 503

--- src/main/java/com/pewue/gh_proxy/decoder/GithubErrorDecoder.java
+++ src/main/java/com/pewue/gh_proxy/decoder/GithubErrorDecoder.java
@@ -1,12 +1,15 @@
 package com.pewue.gh_proxy.decoder;
 
 import com.pewue.gh_proxy.exception.RepositoryNotFoundException;
+import feign.FeignException;
 import feign.Response;
+import feign.RetryableException;
 import feign.codec.ErrorDecoder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpServerErrorException;
 
 public class GithubErrorDecoder implements ErrorDecoder {
+    private final ErrorDecoder defaultErrorDecoder = new ErrorDecoder.Default();
 
     @Override
     public Exception decode(String s, Response response) {
@@ -15,8 +18,18 @@ public class GithubErrorDecoder implements ErrorDecoder {
                 return new RepositoryNotFoundException("Repository not found");
             case 500:
                 return new HttpServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR, "Github unexpected error");
+            case 503:
+                FeignException exception = feign.FeignException.errorStatus(s, response);
+                return new RetryableException(
+                        response.status(),
+                        exception.getMessage(),
+                        response.request().httpMethod(),
+                        exception,
+                        50L,
+                        response.request()
+                );
             default:
-                return new Exception("Unexpected error");
+                return defaultErrorDecoder.decode(s, response);
         }
     }
 }
