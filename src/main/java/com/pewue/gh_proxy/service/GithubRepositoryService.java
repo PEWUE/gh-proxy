06160c8958a256480f23e70b97d34026dadb2ec1
Paweł Kopeć <pawel.kopec13@gmail.com>
2025-10-12 21:02:57 +0200

    exception

--- src/main/java/com/pewue/gh_proxy/service/GithubRepositoryService.java
+++ src/main/java/com/pewue/gh_proxy/service/GithubRepositoryService.java
@@ -2,13 +2,14 @@ package com.pewue.gh_proxy.service;
 
 import com.pewue.gh_proxy.client.GithubClient;
 import com.pewue.gh_proxy.dto.GithubRepositoryResponse;
+import com.pewue.gh_proxy.exception.RepositoryAlreadyExistsException;
 import com.pewue.gh_proxy.exception.RepositoryNotFoundException;
 import com.pewue.gh_proxy.mapper.GithubRepositoryMapper;
 import com.pewue.gh_proxy.model.GHRepository;
 import com.pewue.gh_proxy.repository.GithubRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +24,9 @@ public class GithubRepositoryService {
 
     @Transactional
     public GHRepository add(String owner, String repo) {
+        if (githubRepository.findByFullName(getFullName(owner, repo)).isPresent()) {
+            throw new RepositoryAlreadyExistsException("Repository " + repo + " already exists");
+        }
         GHRepository repository = githubRepositoryMapper.toEntity(githubClient.getGithubRepo(owner, repo));
         return githubRepository.save(repository);
     }
